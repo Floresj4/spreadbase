eb4ac9ae5567ac8ce69610d53dff063609eaa027
Jason <floresj4@gmail.com>
2016-08-07 19:51:44 -0400

    starting type change testing and using static import

--- src/test/java/com/flores/h2/spreadbase/TestColumnRules.java
+++ src/test/java/com/flores/h2/spreadbase/TestColumnRules.java
@@ -1,12 +1,11 @@
 package com.flores.h2.spreadbase;
 
 import static org.junit.Assert.*;
+import static com.flores.h2.spreadbase.analyze.WorkbookAnalyzer.*;
 
 import org.junit.Test;
 
-import com.flores.h2.spreadbase.analyze.WorkbookAnalyzer;
 import com.flores.h2.spreadbase.model.IColumn;
-import com.flores.h2.spreadbase.model.impl.Column;
 import com.flores.h2.spreadbase.util.BuilderUtil;
 
 /**
@@ -18,32 +17,86 @@ public class TestColumnRules {
 	@Test
 	public void testColumnCreation() {
 		//create String
-		IColumn column = WorkbookAnalyzer.makeColumn("xxxxx");
-		assertNotNull(column);
-		assertTrue(column.getType().getTypeName().equals(String.class.getTypeName()));
-		assertEquals(5, column.getPrecision());
-		assertEquals(BuilderUtil.UNSET_INT, column.getScale());
+		IColumn testcol = makeColumn("xxxxx");
+		assertNotNull(testcol);
+		assertTrue(testcol.getType().getTypeName().equals(String.class.getTypeName()));
+		assertEquals(5, testcol.getPrecision());
+		assertEquals(BuilderUtil.UNSET_INT, testcol.getScale());
 		
 		//create Double
-		column = WorkbookAnalyzer.makeColumn("1000.23");
-		assertNotNull(column);
-		assertTrue(column.getType().getTypeName().equals(Double.class.getTypeName()));
-		assertEquals(4, column.getPrecision());
-		assertEquals(2, column.getScale());
+		testcol = makeColumn("1000.23");
+		assertNotNull(testcol);
+		assertTrue(testcol.getType().getTypeName().equals(Double.class.getTypeName()));
+		assertEquals(4, testcol.getPrecision());
+		assertEquals(2, testcol.getScale());
 		
 		//create Integer
-		column = WorkbookAnalyzer.makeColumn("1000");
-		assertNotNull(column);
-		assertTrue(column.getType().getTypeName().equals(Integer.class.getTypeName()));
-		assertEquals(1000, column.getPrecision());
-		assertEquals(BuilderUtil.UNSET_INT, column.getScale());
+		testcol = makeColumn("1000");
+		assertNotNull(testcol);
+		assertTrue(testcol.getType().getTypeName().equals(Integer.class.getTypeName()));
+		assertEquals(1000, testcol.getPrecision());
+		assertEquals(BuilderUtil.UNSET_INT, testcol.getScale());
 	}
 
 	@Test
 	public void testResizeString() {
-		String testval = "xxxxx";
-		IColumn col = new Column(String.class, 4);
-		assertEquals(testval.length(), WorkbookAnalyzer.adjustColumn(col, testval)
-				.getPrecision());
+		String testval = "xxxxx";	//precision should be adjusted
+		IColumn testcol = makeColumn(testval);
+		assertEquals(5, testcol.getPrecision());
+		assertEquals(10, adjustColumn(
+				testcol, "yyyyyyyyyy").getPrecision());
+		
+		testval = "xxx";	//precision should not be adjusted
+		testcol = makeColumn(testval);
+		assertEquals(3, testcol.getPrecision());
+		assertEquals(3, adjustColumn(
+				testcol, "y").getPrecision());
+	}
+	
+	@Test
+	public void testNumberResize() {
+		String testval = "10";	//precision should be adjusted
+		IColumn testcol = makeColumn(testval);
+		assertEquals(10, testcol.getPrecision());
+		assertEquals(100, adjustColumn(
+				testcol, "100").getPrecision());
+		
+		testval = "125";	//precision should not be adjusted
+		testcol = makeColumn(testval);
+		assertEquals(125, testcol.getPrecision());
+		assertEquals(125, adjustColumn(
+				testcol, "120").getPrecision());
+
+		testval = "58.27";	//precision should be adjusted
+		testcol = makeColumn(testval);
+		assertEquals(2, testcol.getPrecision());
+		assertEquals(2, testcol.getScale());
+		
+		IColumn outCol = null;	//scale or precision should change
+		outCol = adjustColumn(testcol, "262.333");
+		assertEquals(3, outCol.getPrecision());
+		assertEquals(3, outCol.getScale());
+		
+		//scale or precision should not change
+		outCol = adjustColumn(testcol, "1.1");
+		assertEquals(2, outCol.getPrecision());
+		assertEquals(2, outCol.getScale());
+
+		//only one attribute changes
+		outCol = adjustColumn(testcol, "224565.22");
+		assertEquals(6, outCol.getPrecision());
+		assertEquals(2, outCol.getScale());
+	}
+	
+	@Test
+	public void testTypeChange() {
+		String testval = "1";	//precision should be adjusted
+		IColumn testcol = makeColumn(testval);
+		assertTrue(testcol.getType().getTypeName().equals(Integer.class.getTypeName()));
+		assertEquals(1, testcol.getPrecision());
+		assertEquals(BuilderUtil.UNSET_INT, testcol.getScale());
+		
+		testcol = makeColumn("22.2");
+		
 	}
 }
